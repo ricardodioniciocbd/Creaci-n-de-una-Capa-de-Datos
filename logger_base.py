ad689fb84092435029dd20ce59142dcc2da2b46e
Ricardo Dionicio <dricardooc4@gmail.com>
2025-08-13 10:57:16 -0600

    Manejo de Logging en Python

--- logger_base.py
+++ logger_base.py
@@ -1,7 +1,7 @@
 # MANEJO DE LOGGING
 import logging as log
 
-# handler  = Manejador. Es un recurso donde vamos a mandar esta informacion
+# handler  = Manejador. Es un recurso donde vamos a mandar esta informacion, sirve para registrar mensajes sobre la ejecuacion del programa (debug, info, warning, error, critical), ayudan a depurar, monitorear errores o eventos en archivos o consla
 log.basicConfig(level=log.DEBUG,
                 format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
                 datefmt='%I:%M:%S %p',
