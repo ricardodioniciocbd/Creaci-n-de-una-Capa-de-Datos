8307bb150788da64e92fd65f6ee65f3382e3b251
Ricardo Dionicio <dricardooc4@gmail.com>
2025-08-16 20:32:38 -0600

    Clase usuario-metodos get y set

---
+++ usuario.py
@@ -0,0 +1,41 @@
+from logger_base import log
+
+class Usuario:
+    def __init__(self, id_usuario = None, username = None, password = None):
+        self._id_usuario = id_usuario
+        self._username = username
+        self._password = password
+
+    def __str__(self):
+        return f'''
+            id usuario {self._id_usuario}, username: {self._username}, password: {self._password}
+        '''
+    @property
+    def id_usuario(self):
+        return self._id_usuario
+
+    @id_usuario.setter
+    def id_usuario(self, id_usuario):
+        self._id_usuario = id_usuario
+    
+    @property
+    def username(self):
+        return self._username
+
+    @username.setter
+    def username(self,username):
+        self._username = username
+
+    @property
+    def password(self):
+        return self._password
+
+    @password.setter
+    def password(self,password):
+        
+        self._password = password
+
+if __name__ == '__main__':
+    # Simular un insert
+    usuario1 = Usuario(username='ness', password='nes123')
+    log.debug(usuario1)
