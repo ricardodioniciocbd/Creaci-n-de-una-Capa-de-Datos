c0c0360c9ec7ff323b8bf5070b3d6a4c2b8908bb
Ricardo Dionicio <dricardooc4@gmail.com>
2025-08-16 20:38:23 -0600

    logger_base Level INFO

--- logger_base.py
+++ logger_base.py
@@ -2,7 +2,7 @@
 import logging as log
 
 # handler  = Manejador. Es un recurso donde vamos a mandar esta informacion, sirve para registrar mensajes sobre la ejecuacion del programa (debug, info, warning, error, critical), ayudan a depurar, monitorear errores o eventos en archivos o consla
-log.basicConfig(level=log.DEBUG,
+log.basicConfig(level=log.INFO,
                 format='%(asctime)s: %(levelname)s [%(filename)s:%(lineno)s] %(message)s',
                 datefmt='%I:%M:%S %p',
                 handlers=[
